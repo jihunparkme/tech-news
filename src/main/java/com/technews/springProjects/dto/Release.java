101701dabe1c85e4fcc4348dd27ee397c3b4f22d
Jihun Park <jihunpark.tech@gmail.com>
2024-04-22 10:17:12 +0900

    test: Cloud Firestore Test

---
+++ src/main/java/com/technews/springProjects/dto/Release.java
@@ -0,0 +1,18 @@
+package com.technews.springProjects.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Release {
+    private String id;
+    private String project;
+    private String version;
+    private String date;
+    private String url;
+}
