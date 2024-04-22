74edf1cdaebc2048680e85942eed43b609d96e2f
Jihun Park <jihunpark.tech@gmail.com>
2024-04-22 22:28:31 +0900

    feat: edit HomeController

--- src/main/java/com/technews/aggregate/index/HomeController.java
+++ src/main/java/com/technews/aggregate/index/HomeController.java
@@ -1,14 +1,18 @@
 package com.technews.aggregate.index;
 
+import com.technews.common.constant.Categories;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class HomeController {
+
     @GetMapping
-    public String home() {
+    public String home(Model model) {
+        model.addAttribute("categories", Categories.values());
         return "index";
     }
 }
