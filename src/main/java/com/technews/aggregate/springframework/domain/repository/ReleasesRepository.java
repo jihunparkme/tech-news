6a8fde6427a9e46d269ac0876fc31a745ca9b358
Jihun Park <jihunpark.tech@gmail.com>
2024-04-22 16:41:45 +0900

    refactor: inline global field

--- src/main/java/com/technews/aggregate/springframework/domain/repository/ReleasesRepository.java
+++ src/main/java/com/technews/aggregate/springframework/domain/repository/ReleasesRepository.java
@@ -1,7 +1,10 @@
 package com.technews.aggregate.springframework.domain.repository;
 
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.*;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Query;
+import com.google.cloud.firestore.QueryDocumentSnapshot;
+import com.google.cloud.firestore.QuerySnapshot;
 import com.google.firebase.cloud.FirestoreClient;
 import com.technews.aggregate.springframework.domain.Release;
 import com.technews.aggregate.springframework.dto.SaveReleaseRequest;
@@ -17,18 +20,17 @@ import java.util.List;
 @Repository
 public class ReleasesRepository {
 
-    private static final Firestore FIRE_STORE = FirestoreClient.getFirestore();
     private static final String COLLECTION_NAME = Collections.RELEASES.key();
 
     public void insertRelease(SaveReleaseRequest saveReleaseRequest) {
-        Query query = FIRE_STORE.collection(COLLECTION_NAME)
+        Query query = FirestoreClient.getFirestore().collection(COLLECTION_NAME)
                 .whereEqualTo("project", saveReleaseRequest.getProject())
                 .whereEqualTo("version", saveReleaseRequest.getVersion());
 
         ApiFuture<QuerySnapshot> querySnapshot = query.get();
 
         if (isNotExistDocument(querySnapshot)) {
-            DocumentReference document = FIRE_STORE.collection(COLLECTION_NAME).document();
+            DocumentReference document = FirestoreClient.getFirestore().collection(COLLECTION_NAME).document();
             final Release release = saveReleaseRequest.toRelease(document.getId());
             document.set(release);
             log.info("new release document added. document ID: {}", document.getId());
@@ -40,7 +42,7 @@ public class ReleasesRepository {
 
     public List<Release> findAllReleases() {
         try {
-            ApiFuture<QuerySnapshot> future = FIRE_STORE.collection(COLLECTION_NAME).get();
+            ApiFuture<QuerySnapshot> future = FirestoreClient.getFirestore().collection(COLLECTION_NAME).get();
             List<QueryDocumentSnapshot> documents = future.get().getDocuments();
             List<Release> result = new ArrayList<>();
             for (QueryDocumentSnapshot document : documents) {
