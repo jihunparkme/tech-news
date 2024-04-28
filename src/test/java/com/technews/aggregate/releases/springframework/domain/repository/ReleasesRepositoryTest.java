01bf3bbb9518c6add61736c2c2b1d6ae7e3c07c6
Jihun Park <jihunpark.tech@gmail.com>
2024-04-28 21:36:22 +0900

    test: find all pageable

--- src/test/java/com/technews/aggregate/releases/springframework/domain/repository/ReleasesRepositoryTest.java
+++ src/test/java/com/technews/aggregate/releases/springframework/domain/repository/ReleasesRepositoryTest.java
@@ -5,6 +5,9 @@ import com.technews.aggregate.releases.springframework.dto.ReleaseResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.ActiveProfiles;
 
 import java.util.List;
@@ -25,10 +28,27 @@ class ReleasesRepositoryTest {
     }
 
     @Test
-    void test() {
+    void findByProjectOrderAllByVersionDescLimitOne() {
         final List<Release> releases = releasesRepository.findByProjectOrderByVersionDescLimitOne("spring-framework");
 
         final List<ReleaseResponse> result = releases.stream().
+                map(release -> ReleaseResponse.of(release))
+                .collect(Collectors.toList());
+
+        result.forEach(System.out::println);
+    }
+
+    @Test
+    void find_all_pageable() {
+        final int page = 1;
+        final int size = 10;
+        final PageRequest pageable = PageRequest.of(
+                page, size,
+                Sort.by("createdDt").descending().and(Sort.by("version").descending()));
+
+        final Page<Release> releases = releasesRepository.findAll(pageable);
+
+        final List<ReleaseResponse> result = releases.getContent().stream().
                 map(release -> ReleaseResponse.of(release))
                 .collect(Collectors.toList());
 
