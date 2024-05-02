bbfb1bec180e70cc71f6f85903110597a5b885c5
Jihun Park <jihunpark.tech@gmail.com>
2024-05-02 10:27:08 +0900

    feat: add query method in repository

--- src/main/java/com/technews/aggregate/releases/springframework/domain/repository/ReleasesRepository.java
+++ src/main/java/com/technews/aggregate/releases/springframework/domain/repository/ReleasesRepository.java
@@ -1,6 +1,8 @@
 package com.technews.aggregate.releases.springframework.domain.repository;
 
 import com.technews.aggregate.releases.springframework.domain.Release;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
@@ -14,4 +16,8 @@ public interface ReleasesRepository extends MongoRepository<Release, String> {
             "{ '$limit' : 1 }"
     })
     List<Release> findByProjectOrderByVersionDescLimitOne(final String project);
+
+    Page<Release> findAll(Pageable pageable);
+
+    Page<Release> findByProjectIn(List<String> project, Pageable pageable);
 }
