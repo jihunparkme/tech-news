6ada3848681217216348c6b526fb211d82b2781c
Jihun Park <jihunpark.tech@gmail.com>
2024-05-03 17:29:18 +0900

    test: isLatestVersion method

--- src/test/java/com/technews/aggregate/releases/springframework/dto/SaveReleaseRequestTest.java
+++ src/test/java/com/technews/aggregate/releases/springframework/dto/SaveReleaseRequestTest.java
@@ -33,4 +33,21 @@ class SaveReleaseRequestTest {
         assertFalse(release.isLatestDateVersion("Apr 19, 2024"));
         assertFalse(release.isLatestDateVersion("Apr 20, 2024"));
     }
+
+    @Test 
+    void when_isLatestVersion_then_return_true() {
+        final SaveReleaseRequest release = SaveReleaseRequest.builder()
+                .project(PROJECT)
+                .version("JDK 8u74")
+                .date("Apr 19, 2024")
+                .url("url")
+                .build();
+
+        assertTrue(release.isLatestVersion("JDK 8u92"));
+        assertTrue(release.isLatestVersion("JDK 8u91"));
+        assertTrue(release.isLatestVersion("JDK 8u77"));
+        assertFalse(release.isLatestVersion("JDK 8u74"));
+        assertFalse(release.isLatestVersion("JDK 8u71"));
+        assertFalse(release.isLatestVersion(""));
+    }
 }
