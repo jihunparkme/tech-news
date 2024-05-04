40985d62d63b42eac75e8fc2ccf531d105fec1e9
Jihun Park <jihunpark.tech@gmail.com>
2024-05-04 16:43:56 +0900

    feat: edit domain

--- src/main/java/com/technews/aggregate/posts/domain/Post.java
+++ src/main/java/com/technews/aggregate/posts/domain/Post.java
@@ -1,7 +1,28 @@
 package com.technews.aggregate.posts.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
+
+@Document(collection = "posts")
+@Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Post {
+    public static final Post EMPTY = new Post();
+
+    private String id;
+    private String subject;
     private String title;
-    private Meta meta;
     private String url;
+
+    private String category;
+    private String writer;
+    private String date;
+    List<String> tags;
 }
