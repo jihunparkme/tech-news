e12ba9f54707c63454ddb5cbaaef2dd07e950f3b
Jihun Park <jihunpark.tech@gmail.com>
2024-05-04 16:44:15 +0900

    feat: add repository method

--- src/main/java/com/technews/aggregate/posts/domain/repository/PostsRepository.java
+++ src/main/java/com/technews/aggregate/posts/domain/repository/PostsRepository.java
@@ -3,8 +3,19 @@ package com.technews.aggregate.posts.domain.repository;
 import com.technews.aggregate.posts.domain.Post;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.mongodb.repository.Aggregation;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostsRepository extends MongoRepository<Post, String> {
+
+    @Aggregation(pipeline = {
+            "{ '$match': { 'category' : ?0 } }",
+            "{ '$sort' : { 'date' : -1 } }",
+            "{ '$limit' : 1 }"
+    })
+    List<Post> findByCategoryOrderByDateDescLimitOne(String category);
+
     Page<Post> findAll(Pageable pageable);
 }
