fbbee6d69c736f14adc6b166740d74a273622cd1
Jihun Park <jihunpark.tech@gmail.com>
2024-12-20 23:02:10 +0900

    refactor: model add attribute

--- src/main/kotlin/com/technews/aggregate/index/HomeController.kt
+++ src/main/kotlin/com/technews/aggregate/index/HomeController.kt
@@ -5,6 +5,7 @@ import com.technews.aggregate.posts.constant.SpringBlogsSubject
 import com.technews.aggregate.releases.constant.Categories
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 
@@ -14,19 +15,19 @@ class HomeController {
 
     @GetMapping
     fun home(model: Model): String {
-        model.addAttribute("categories", Categories.entries.toTypedArray())
+        model["categories"] = Categories.entries.toTypedArray()
         return "index"
     }
 
     @GetMapping("/blog/spring")
     fun springPosts(model: Model): String {
-        model.addAttribute("categories", SpringBlogsSubject.entries.toTypedArray())
+        model["categories"] = SpringBlogsSubject.entries.toTypedArray()
         return "blog/spring"
     }
 
     @GetMapping("/blog/java")
     fun JavaPosts(model: Model): String {
-        model.addAttribute("categories", JavaBlogsSubject.entries.toTypedArray())
+        model["categories"] = JavaBlogsSubject.entries.toTypedArray()
         return "blog/java"
     }
 }
