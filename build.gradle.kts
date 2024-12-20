8a00b0b9a10f1f4d5ede50fc7f248135f4cf8d91
Jihun Park <jihunpark.tech@gmail.com>
2024-12-20 22:48:12 +0900

    refactor: remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,7 +32,6 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-reflect")
 
     // data
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
 
     // Utility libraries
