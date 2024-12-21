1f58afeb50b85f2fbb71333f446adec87e7febbd
Jihun Park <jihunpark.tech@gmail.com>
2024-12-21 22:25:33 +0900

    refactor: java to kotlin (posts repository, service)

---
+++ src/main/kotlin/com/technews/aggregate/posts/service/PostsService.kt
@@ -0,0 +1,26 @@
+package com.technews.aggregate.posts.service
+
+import com.technews.aggregate.posts.constant.PostSubjects
+import com.technews.aggregate.posts.domain.Post
+import com.technews.aggregate.posts.domain.repository.PostsRepository
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.PageRequest
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class PostsService(
+    private val postsRepository: PostsRepository
+) {
+    @Transactional(readOnly = true)
+    fun findAllRelease(
+        subject: PostSubjects,
+        pageable: PageRequest,
+        categories: List<String>?
+    ): Page<Post> =
+        if (categories.isNullOrEmpty()) {
+            postsRepository.findBySubject(subject.value, pageable)
+        } else {
+            postsRepository.findBySubjectAndCategoryIn(subject.value, categories, pageable)
+        }
+}
