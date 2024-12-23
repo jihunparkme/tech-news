a3d232a0788ea60052514dd2227310ca026dfae3
Jihun Park <jihunpark.tech@gmail.com>
2024-12-23 15:50:20 +0900

    refactor: java to kotlin (common constant)

---
+++ src/main/kotlin/com/technews/common/constant/Collections.kt
@@ -0,0 +1,9 @@
+package com.technews.common.constant
+
+enum class Collections(
+    val group: String,
+    val key: String,
+) {
+    RELEASES("spring-projects", "RELEASES"),
+    ;
+}
---
+++ src/main/kotlin/com/technews/common/constant/Result.kt
@@ -0,0 +1,8 @@
+package com.technews.common.constant
+
+enum class Result(
+    val message: String,
+) {
+    FAIL("FAIL"),
+    SUCCESS("SUCCESS");
+}
