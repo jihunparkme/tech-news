5e03fda1844a10f30b845a1e48a07ee060a24ca9
Jihun Park <jihunpark.tech@gmail.com>
2024-12-27 13:19:38 +0900

    test: apply kotlin style

---
+++ src/test/java/com/technews/aggregate/releases/dto/SaveReleaseRequestTest.kt
@@ -0,0 +1,31 @@
+package com.technews.aggregate.releases.dto
+
+import com.technews.aggregate.releases.createReleaseRequest
+import io.kotest.assertions.assertSoftly
+import io.kotest.core.spec.style.StringSpec
+import io.kotest.matchers.booleans.shouldBeFalse
+import io.kotest.matchers.booleans.shouldBeTrue
+
+class SaveReleaseRequestTest : StringSpec({
+    "마지막 일자의 릴리즈 버전인지 확인" {
+        val request = createReleaseRequest(date = "Dec 25, 2024")
+
+        assertSoftly(request) {
+            request.isLatestDateVersion("").shouldBeTrue()
+            request.isLatestDateVersion("Dec 24, 2024").shouldBeTrue()
+            request.isLatestDateVersion("Dec 25, 2024").shouldBeFalse()
+            request.isLatestDateVersion("2024-12-25").shouldBeFalse()
+        }
+    }
+
+    "" {
+        val request = createReleaseRequest(date = "Dec 25, 2024")
+
+        assertSoftly(request) {
+            request.isLatestDateVersion("").shouldBeTrue()
+            request.isLatestDateVersion("Dec 24, 2024").shouldBeTrue()
+            request.isLatestDateVersion("Dec 25, 2024").shouldBeFalse()
+            request.isLatestDateVersion("2024-12-25").shouldBeFalse()
+        }
+    }
+})
