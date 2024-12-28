3a4bb562ec51a269e13ac2c58e0b73918bfa1fea
Jihun Park <jihunpark.tech@gmail.com>
2024-12-28 19:55:41 +0900

    refactor: rename method

--- src/test/java/com/technews/scheduler/BlogJavaInsideSchedulerTest.kt
+++ src/test/java/com/technews/scheduler/BlogJavaInsideSchedulerTest.kt
@@ -3,17 +3,19 @@ package com.technews.scheduler
 import com.technews.IntegrationTest
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
+import org.junit.jupiter.api.Disabled
 import org.springframework.beans.factory.annotation.Autowired
 
+@Disabled
 @IntegrationTest
 class BlogJavaInsideSchedulerTest(
     @Autowired private val blogJavaInsideScheduler: BlogJavaInsideScheduler
 ) : StringSpec({
-    "Inside Java 게시물 스크래핑 동작" {
+    "scrape java inside post" {
         blogJavaInsideScheduler.runScheduled()
     }
 
-    "게시물 작성자와 날짜 파싱" {
+    "parse the post writer and date" {
         val postInfo = "Joakim Nordström on April 12, 2024"
         val split = postInfo.split(" on ")
 
@@ -21,7 +23,7 @@ class BlogJavaInsideSchedulerTest(
         split[1] shouldBe "April 12, 2024"
     }
 
-    "게시물 날짜 파싱" {
+    "parse the post date" {
         val postInfo = "April 12, 2024"
         val split = postInfo.split(" on ")
 
