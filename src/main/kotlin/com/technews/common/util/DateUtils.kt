3a0ff8aa9a3c7580ce721f75603a9b699eda652d
Jihun Park <jihunpark.tech@gmail.com>
2024-12-31 22:22:57 +0900

    feat: add gmtToSeoul method

--- src/main/kotlin/com/technews/common/util/DateUtils.kt
+++ src/main/kotlin/com/technews/common/util/DateUtils.kt
@@ -2,6 +2,8 @@ package com.technews.common.util
 
 import mu.KotlinLogging
 import java.time.LocalDate
+import java.time.ZoneId
+import java.time.ZonedDateTime
 import java.time.format.DateTimeFormatter
 import java.time.format.DateTimeParseException
 import java.util.Locale
@@ -12,10 +14,22 @@ object DateUtils {
     val ENGLISH_FORMATTER: DateTimeFormatter =
         DateTimeFormatter.ofPattern("[MMMM dd, yyyy][MMMM d, yyyy][MMM dd, yyyy][MMM d, yyyy]", Locale.ENGLISH)
 
+    val GREENWICH_FORMATTER: DateTimeFormatter =
+        DateTimeFormatter.ofPattern("[EEE, dd MMM yyyy HH:mm:ss z][EEE, d MMM yyyy HH:mm:ss z]", Locale.ENGLISH)
+
     val CREATED_FORMATTER: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
 
     fun getFormattedDate(date: String): String = try {
         LocalDate.parse(date, ENGLISH_FORMATTER).format(CREATED_FORMATTER)
+    } catch (e: DateTimeParseException) {
+        logger.error(e) { "Failed to parse date: $date" }
+        LocalDate.now().format(CREATED_FORMATTER)
+    }
+
+    fun gmtToSeoul(date: String): String = try {
+        val zonedDateTime = ZonedDateTime.parse(date, GREENWICH_FORMATTER)
+        val withZoneSameInstant = zonedDateTime.withZoneSameInstant(ZoneId.of("Asia/Seoul"))
+        withZoneSameInstant.format(CREATED_FORMATTER)
     } catch (e: DateTimeParseException) {
         logger.error(e) { "Failed to parse date: $date" }
         LocalDate.now().format(CREATED_FORMATTER)
--- src/test/java/com/technews/common/util/DateUtilsTest.kt
+++ src/test/java/com/technews/common/util/DateUtilsTest.kt
@@ -1,12 +1,13 @@
 package com.technews.common.util
 
 import com.technews.common.util.DateUtils.getFormattedDate
+import com.technews.common.util.DateUtils.gmtToSeoul
 import io.kotest.core.spec.style.StringSpec
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Assertions
 
 class DateUtilsTest : StringSpec({
-    "when_formatted_date_then_true" {
+    "when_input_english_date_parse_then_true" {
         var result = getFormattedDate("Apr 18, 2024")
         result shouldBe "2024-04-18"
 
@@ -25,5 +26,16 @@ class DateUtilsTest : StringSpec({
         result = getFormattedDate("April 9, 2024")
         Assertions.assertEquals("2024-04-09", result)
         result shouldBe "2024-04-09"
+    }
+
+    "when_input_greenwich_date_parse_then_true" {
+        var result = gmtToSeoul("Tue, 10 Dec 2024 17:55:00 GMT")
+        result shouldBe "2024-12-11"
+
+        result = gmtToSeoul("Tue, 15 Oct 2024 19:42:00 GMT")
+        result shouldBe "2024-10-16"
+
+        result = gmtToSeoul("Tue, 7 Nov 2023 18:30:00 GMT")
+        result shouldBe "2024-11-08"
     }
 })
