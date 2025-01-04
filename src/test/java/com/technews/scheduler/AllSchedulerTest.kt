9649e8ce95d6bbfde24cc3639fa9f85db224ee6a
Jihun Park <jihunpark.tech@gmail.com>
2025-01-04 21:46:53 +0900

    feat: add releasesKafkaScheduler

--- src/test/java/com/technews/scheduler/AllSchedulerTest.kt
+++ src/test/java/com/technews/scheduler/AllSchedulerTest.kt
@@ -6,6 +6,7 @@ import com.technews.scheduler.blog.BlogOracleJavaRssScheduler
 import com.technews.scheduler.blog.BlogSpringScheduler
 import com.technews.scheduler.releases.ReleaseSpringProjectsScheduler
 import com.technews.scheduler.releases.ReleasesJavaScheduler
+import com.technews.scheduler.releases.ReleasesKafkaScheduler
 import io.kotest.core.spec.style.StringSpec
 import org.junit.jupiter.api.Disabled
 import org.springframework.beans.factory.annotation.Autowired
@@ -15,17 +16,23 @@ import org.springframework.beans.factory.annotation.Autowired
 class AllSchedulerTest(
     @Autowired private val releaseSpringProjectsScheduler: ReleaseSpringProjectsScheduler,
     @Autowired private val releasesJavaScheduler: ReleasesJavaScheduler,
+    @Autowired private val releasesKafkaScheduler: ReleasesKafkaScheduler,
+
     @Autowired private val blogSpringScheduler: BlogSpringScheduler,
     @Autowired private val blogJavaInsideScheduler: BlogJavaInsideScheduler,
+    @Autowired private val blogOracleJavaRssScheduler: BlogOracleJavaRssScheduler,
+
     @Autowired private val subscribeMailScheduler: SubscribeMailScheduler,
-    @Autowired private val javaBlogOracleJavaRssScheduler: BlogOracleJavaRssScheduler,
 ) : StringSpec({
         "scrape all post" {
             releaseSpringProjectsScheduler.runSchedule()
             releasesJavaScheduler.runSchedule()
+            releasesKafkaScheduler.runSchedule()
+
             blogSpringScheduler.runSchedule()
             blogJavaInsideScheduler.runSchedule()
+            blogOracleJavaRssScheduler.runSchedule()
+
             subscribeMailScheduler.runSchedule()
-            javaBlogOracleJavaRssScheduler.runSchedule()
         }
     })
