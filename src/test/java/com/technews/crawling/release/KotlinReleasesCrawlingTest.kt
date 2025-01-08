15f969e5fcdee8fd7e46e250ea051c0f6e8ec8bf
Jihun Park <jihunpark.tech@gmail.com>
2025-01-08 21:31:08 +0900

    test: KotlinReleasesCrawlingTest

---
+++ src/test/java/com/technews/crawling/release/KotlinReleasesCrawlingTest.kt
@@ -0,0 +1,28 @@
+package com.technews.crawling.release
+
+import io.kotest.core.spec.style.BehaviorSpec
+import mu.KotlinLogging
+import org.jsoup.Jsoup
+
+private val logger = KotlinLogging.logger {}
+
+class KotlinReleasesCrawlingTest : BehaviorSpec({
+    Given("kafka releases 정보 크롤링") {
+        Jsoup.connect(BASE_RELEASES_URL)
+            .get()
+            .select("section")
+            .map {
+                println("title : " + it.select("relative-time").attr("datetime"))
+                println("version : " + it.select(".Box-body").select("a").select(".Link--primary").text())
+                println("label : " + it.select(".Box-body").select("flex-column"))
+                println("url : " + GITHUB_URL + it.select(".Box-body").select("a").select(".Link--primary").attr("href"))
+                println()
+            }
+            .forEach { println(it) }
+    }
+}) {
+    companion object {
+        private const val BASE_RELEASES_URL = "https://github.com/JetBrains/kotlin/releases"
+        private const val GITHUB_URL = "https://github.com/"
+    }
+}
